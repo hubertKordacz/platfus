667cdca7c779977af9e2fafac991504d0f37ad14
DESKTOP-S8SHC68\wolfm <wolfmar.ww@gmail.com>
2018-05-11 23:53:01 +0200

    fiz ggit ignore

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerInput : MonoBehaviour {
+
+	public enum PlayerId
+	{
+		player1=0,
+        player2=1,
+        player3=2,
+        player4=3
+	}
+	public  enum InputActions
+	{
+		Vertical = 0,
+		Horizontal = 1,
+		Attack =2
+    }
+    
+	public PlayerId playerId = PlayerId.player1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log("Axis " + GetAxis(InputActions.Vertical));
+		Debug.Log("Attack " + GetButton(InputActions.Attack));
+	}
+	private Player rewiredPlayer
+	{
+		get
+		{
+			return ReInput.players.GetPlayer((int)playerId);
+		}
+	}
+	public bool GetButtonDown(InputActions input)
+    {      
+		return rewiredPlayer.GetButtonDown(input.ToString());
+    }
+
+	public bool GetButtonUp(InputActions input)
+    {
+
+		return rewiredPlayer.GetButtonUp(input.ToString());
+    }
+
+    public bool GetButton(InputActions input)
+    {
+
+        return rewiredPlayer.GetButton(input.ToString());
+    }
+
+	public float GetAxis(InputActions input)
+    {
+		return rewiredPlayer.GetAxis(input.ToString());
+    }
+}
