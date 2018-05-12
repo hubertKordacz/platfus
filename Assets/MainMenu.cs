98746b80a6d3721624d928f95fd2d69c6ea698ee
DESKTOP-S8SHC68\wolfm <wolfmar.ww@gmail.com>
2018-05-12 16:53:36 +0200

    mqain menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Image[] background;
+    public float backgroundMoveSpeed;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for (int i = 0; i < background.Length; i++)
+        //{
+        //    background[i].transform.position += Vector3.left * backgroundMoveSpeed * Time.deltaTime;
+        //    if (background[i].rectTransform.anchoredPosition.x < -background[i].rectTransform.sizeDelta.x)
+        //    {
+        //        background[i].rectTransform.anchoredPosition = new Vector3(background[i].rectTransform.sizeDelta.x, 0, 0);
+        //    }
+        //}
+    }
+}
