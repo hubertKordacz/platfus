60c25d42abb66005bdb85eb3a97f376e46a877b1
DESKTOP-S8SHC68\wolfm <wolfmar.ww@gmail.com>
2018-05-12 04:11:37 +0200

    Movement Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(Collider))]
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
-    public float rotationSpeed;
+    public float moveSpeed = 50;
+    public float rotationSpeed = 500;
+
+    public float minChargeForce = 50, maxChargeForce = 500;
 
     Rigidbody _rigidbody;
     Collider _collider;
+    float _chargeForceTimer;
+    bool _isCharging;
 
     public Rigidbody Rigidbody { get { { return _rigidbody; } } }
     public Collider Collider { get { { return _collider; } } }
@@ -18,27 +21,56 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _collider = GetComponent<Collider>();
+        _collider = GetComponentInChildren<Collider>();
     }
 
     private void Update()
     {
-        var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-        Move(move);
-
-        if (_rigidbody.velocity.x != 0 && _rigidbody.velocity.y != 0)
+        if (_isCharging)
         {
-            //transform.eulerAngles = 
-            Debug.Log(_rigidbody.velocity);
-            transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);
+            if (_rigidbody.velocity == Vector3.zero)
+                _isCharging = false;
+            return;
         }
+
+        Move();
+        Rotate();
+        Charge();
     }
 
-    void Move(Vector3 force)
+    void Move()
     {
-        _rigidbody.AddForce(force * moveSpeed);
+        var move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        var lerp = 1 - _chargeForceTimer;
+        _rigidbody.AddForce(move * moveSpeed * lerp);
+    }
 
+    void Rotate()
+    {
+        if (_rigidbody.velocity == Vector3.zero) return;
 
+        Quaternion wantedRotation = Quaternion.LookRotation(_rigidbody.velocity, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, wantedRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    void Charge()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _isCharging = true;
+
+            var lerp = _chargeForceTimer;
+            var force = Mathf.Lerp(minChargeForce, maxChargeForce, lerp);
+            _rigidbody.velocity = transform.forward * force;
+
+            _chargeForceTimer = 0;
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _chargeForceTimer += Time.deltaTime;
+            _chargeForceTimer = Mathf.Clamp(_chargeForceTimer, 0, 1);
+        }
     }
 }
