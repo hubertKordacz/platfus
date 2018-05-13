4b11d30dd8666c6d2c049837d4dd24a470644ac4
DESKTOP-S8SHC68\wolfm <wolfmar.ww@gmail.com>
2018-05-13 12:37:03 +0200

    scene menagment

--- Assets/SceneManager.cs
+++ Assets/SceneManager.cs
@@ -1,37 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour
 {
+    public Image image;
+
     public PlayerController[] players;
 
     public static bool secondRound = false;
 
-    bool _loading = false;
+    bool _loading, _fading = false;
+    float _timer = 0;
 
     public void Update()
     {
+        if (_fading)
+        {
+            _timer += Time.deltaTime * 2;
+            var color = image.color;
+            color.a = Mathf.Lerp(0, 1, _timer);
+            image.color = color;
+
+            return;
+        }
+
         if (_loading) return;
 
         int alive = 0;
+        int aliveIndex = 0;
 
         for (int i = 0; i < players.Length; i++)
         {
             if (players[i].Equals(null)) continue;
-            if (players[i].gameObject && !players[i].gameObject.activeInHierarchy) continue;
+            if (players[i].gameObject && !players[i].gameObject.activeInHierarchy || players[i].die) continue;
 
             alive++;
+            aliveIndex = i;
         }
 
         if (alive > 1) return;
 
-        _loading = true;
+
+        players[aliveIndex].playerInput.SetPoints();
         StartCoroutine(NextLevel());
     }
 
     IEnumerator NextLevel()
     {
+        _loading = true;
+
+        yield return new WaitForSeconds(0.5f);
+
+        _fading = true;
+
         yield return new WaitForSeconds(1);
 
         StageSellect.levelToLoad++;
